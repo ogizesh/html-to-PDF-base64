9ffb8406507d1e60d09e04d74a86fb0cac4bb50a
ogizesh <39046371+ogizesh@users.noreply.github.com>
2024-08-05 10:15:18 +0300

    Update function.js

--- function.js
+++ function.js
@@ -1,9 +1,9 @@
 window.function = function (html, fileName, format, zoom, orientation, margin, breakBefore, breakAfter, breakAvoid, fidelity, customDimensions) {
-let result = {
+let result = `{
 	success: true,
 	message: "HTML received: " + html.value,
 	base64String: "This is a placeholder"
-    };
+    }`;
 
     return result;
 };
